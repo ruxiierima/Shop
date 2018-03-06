aa05dcf4efa49744a5ac48b6ea994bb6443878fa
chioreandan <chiorean.dan12@gmail.com>
2018-03-07 00:32:03 +0200

    Added UnitOfWork

---
+++ Shop/Shop/Data/IRepositories/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shop.Data.Repositories;
+using Shop.Model;
+
+namespace Shop.Data.IRepositories
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        UserRepository Users { get; }
+        ProductRepository Products { get; }
+        int Complete();
+    }
+}
---
+++ Shop/Shop/Data/UnitOfWork.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shop.Model;
+using Shop.Data.IRepositories;
+using Shop.Data.Repositories;
+
+namespace Shop.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ShopContext _context;
+        
+        public UnitOfWork(ShopContext context)
+        {
+            _context = context;
+            Users = new UserRepository(_context);
+            Products = new ProductRepository(_context);
+
+        }
+        public IUserRepository Users { get; set; }
+        public IProductRepository Products { get; set; }
+
+        UserRepository IUnitOfWork.Users => throw new NotImplementedException();
+
+        ProductRepository IUnitOfWork.Products => throw new NotImplementedException();
+
+        public int Complete()
+        {
+            return _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
