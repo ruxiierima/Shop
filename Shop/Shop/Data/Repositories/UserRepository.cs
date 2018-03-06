d9b2f425e0ef8146d9ccb1c5e6f2a2664c2557b7
chioreandan <chiorean.dan12@gmail.com>
2018-03-06 23:59:00 +0200

    Repository+UserRepository added

---
+++ Shop/Shop/Data/Repositories/UserRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shop.Data.IRepositories;
+using Shop.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Shop.Data.Repositories
+{
+    public class UserRepository : Repository<User>, IUserRepository
+    {
+        public UserRepository(ShopContext context) : base(context)
+        {
+
+        }
+        public User GetUser(int id)
+        {
+            return ShopContext.Users.Include(u => u.Username).SingleOrDefault(u => u.Id == id);
+        }
+
+        public IEnumerable<User> GetUsers(int c)
+        {
+            return ShopContext.Users.ToList();
+        }
+        public ShopContext ShopContext
+        {
+            get { return Context as ShopContext; }
+        }
+    }
+}
