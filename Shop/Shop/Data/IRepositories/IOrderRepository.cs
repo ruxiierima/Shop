829fe0d1cd87ab1862bdaaf80de9e682413ca859
chioreandan <chiorean.dan12@gmail.com>
2018-03-07 01:06:46 +0200

    OrderRepository added

---
+++ Shop/Shop/Data/IRepositories/IOrderRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shop.Model;
+
+namespace Shop.Data.IRepositories
+{
+    public interface IOrderRepository: IRepository<Order>
+    {
+        IEnumerable<Order> GetOrders(int c);
+        Order GetOrder(int id);
+    }
+}
---
+++ Shop/Shop/Data/Repositories/OrderRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Shop.Data.IRepositories;
+using Shop.Model;
+
+namespace Shop.Data.Repositories
+{
+    public class OrderRepository : Repository<Order>, IOrderRepository
+    {
+        public OrderRepository(DbContext context) : base(context)
+        {
+        }
+
+        public Order GetOrder(int id)
+        {
+            return ShopContext.Orders.Include(o => o.Products).SingleOrDefault(u => u.Id == id);
+        }
+
+        public IEnumerable<Order> GetOrders(int c)
+        {
+            throw new NotImplementedException();
+            //TO DO : To implement this method
+        }
+        public ShopContext ShopContext
+        {
+            get { return Context as ShopContext; }
+        }
+    }
+}
